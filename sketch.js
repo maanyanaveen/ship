e8af9a49aae9f416a739a75dc0ec07109eaeb0d9
maanyanaveen <85870661+maanyanaveen@users.noreply.github.com>
2021-06-16 15:56:00 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
